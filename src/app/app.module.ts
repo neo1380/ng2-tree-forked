1fa7e9c48320c35fb495d137911aac555d7ddcda
dineshbabu thoota <dineshbabu.thoota@hotmail.com>
2021-06-21 09:51:45 +0530

    tree forked from ng2 tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TreeModule } from './itreelite';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    TreeModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/itreelite/public-api.ts
@@ -0,0 +1,15 @@
+
+
+
+export * from './tree.component';
+export * from './tree.types';
+export * from './tree-internal.component';
+export * from './draggable/node-draggable.directive';
+export * from './draggable/node-draggable.service';
+export * from  './editable/node-editable.directive';
+export * from './menu/node-menu.component';
+export * from './menu/node-menu.service';
+export * from './tree.service';
+export * from './utils/safe-html.pipe';
+
+export * from './tree.module';
